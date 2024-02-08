06dff3f6290b620fa2ac0954ea40b6e25b4b204e
Jonathan Suru <jonathansuru@icloud.com>
2024-02-08 23:33:32 +0100

    ADD: Get data from wikipedia fon

--- petitgpt/wiki_scrapping.py
+++ petitgpt/wiki_scrapping.py
@@ -1,34 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 
-# specify the url
-get_links = []
-urls = [
-    "https://fon.wikipedia.org/w/index.php?title=Ɖé%C9%96ovo:Toutes_les_pages&from=%27%27Bénoît+af%C9%94t%C9%94n+nukun+%C9%96okpo+g%C9%94%C9%94%27%27",
-    "https://fon.wikipedia.org/w/index.php?title=Ɖé%C9%96ovo:Toutes_les_pages&from=Dakpè+Sossou",
-    "https://fon.wikipedia.org/w/index.php?title=Ɖé%C9%96ovo:Toutes_les_pages&from=K%C9%94nkádà",
-    "https://fon.wikipedia.org/w/index.php?title=Ɖé%C9%96ovo:Toutes_les_pages&from=Tossou+Okri+Pascal"]
-for url in urls:
-    source = requests.get(url)
 
-    # Make a soup
-    soup = BeautifulSoup(source.text, 'lxml')
+def get_full_url(path_):
+    return f"https://fon.wikipedia.org{path_}"
 
-    # get all li in <ul class="mw-allpages-chunk">
 
-    links = soup.find('ul', class_='mw-allpages-chunk')
-    links_ = links.find_all('li')
-    get_links.extend(link.find('a')['href'] for link in links_)
-# get all pages and save in txt file format
-for link in get_links:
-    page = requests.get(f"https://fon.wikipedia.org{link}")
-    # Make a soup
-    soup = BeautifulSoup(page.text, 'lxml')
-    div = soup.find('div', id='mw-content-text')
-    print(div)
-    text = div.text
+# Use a session object for persistent connections
+with requests.Session() as session:
+    urls = [
+        "/w/index.php?title=%C6%89%C3%A9%C9%96ovo:Toutes_les_pages&from=%27%27B%C3%A9no%C3%AEt+af%C9%94t%C9%94n+nukun"
+        "+%C9%96okpo+g%C9%94%C9%94%27%27",
+        "/w/index.php?title=%C6%89%C3%A9%C9%96ovo:Toutes_les_pages&from=Christophe+Adimou",
+        "/w/index.php?title=%C6%89%C3%A9%C9%96ovo:Toutes_les_pages&from=Kpl%C9%94nyiji+alav%C9%94t%C9%9Bn+Agbom%C9%9B"
+        "+kan%C9%96ofi+t%C9%94n",
+        "/w/index.php?title=%C6%89%C3%A9%C9%96ovo:Toutes_les_pages&from=Tokp%C9%94nlavi+Cumi-Cumi+t%C9%94n"
+    ]
+    get_links = []
+    for path in urls:
+        try:
+            source = session.get(get_full_url(path))
+            source.raise_for_status()
+            soup = BeautifulSoup(source.text, 'lxml')
+            links = soup.find('ul', class_='mw-allpages-chunk')
+            get_links.extend(link.find('a')['href'] for link in links.find_all('li'))
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
 
-    # save the text in txt file
-    with open(link.replace("/", "_") + ".txt", "w") as f:
-        f.write(text)
-    print(link)
+    for link in get_links:
+        try:
+            page = session.get(get_full_url(link))
+            page.raise_for_status()
+            soup = BeautifulSoup(page.text, 'lxml')
+            div = soup.find('div', id='mw-content-text')
+            text = div.text if div else ""
+            filename = link.replace("/", "_") + ".txt"
+            with open(filename, "w") as f:
+                f.write(text)
+            print(link)
+        except requests.RequestException as e:
+            print(f"Request failed: {e}")
+        except IOError as e:
+            print(f"File operation failed: {e}")
